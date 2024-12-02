1d0c4278baf386f9021e6734ac8f647221c0e8f3
Sebastian <sebjak1@hotmail.com>
2024-12-02 09:44:12 +0100

    fix

--- src/main/java/keac4/efficio/controller/UserController.java
+++ src/main/java/keac4/efficio/controller/UserController.java
@@ -2,7 +2,7 @@ package keac4.efficio.controller;
 
 import jakarta.servlet.http.HttpSession;
 import keac4.efficio.model.User;
-import keac4.efficio.Service.UserService;
+import keac4.efficio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
