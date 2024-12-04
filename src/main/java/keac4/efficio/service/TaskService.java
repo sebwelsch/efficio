3e3301438202bf2434dab3be64c6b949fe53eea9
sebwelsch <sebjak1@hotmail.com>
2024-12-04 13:41:13 +0100

    add task time to project

--- src/main/java/keac4/efficio/service/TaskService.java
+++ src/main/java/keac4/efficio/service/TaskService.java
@@ -1,6 +1,9 @@
 package keac4.efficio.service;
 
+import keac4.efficio.model.Project;
+import keac4.efficio.model.Subproject;
 import keac4.efficio.model.Task;
+import keac4.efficio.repository.ProjectRepository;
 import keac4.efficio.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,21 +12,25 @@ import org.springframework.stereotype.Service;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository) {
         this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
     }
-
 
     public void saveNewTask(Task task) {
         task.setSubprojectId(task.getSubprojectId());
         taskRepository.saveNewTask(task);
-    }
 
+        Subproject subproject = projectRepository.getSubprojectById(task.getSubprojectId());
+        int newExpectedTimeSubproject = subproject.getExpectedTime() + task.getExpectedTime();
+        projectRepository.updateExpectedTimeSubproject(subproject.getSubprojectId(), newExpectedTimeSubproject);
 
-    public boolean doesUserHaveAccess(int projectId, int userId) {
-        return false;
+        Project project = projectRepository.getProjectById(subproject.getProjectId());
+        int newExpectedTimeProject = project.getExpectedTime() + task.getExpectedTime();
+        projectRepository.updateExpectedTimeProject(project.getProjectId(), newExpectedTimeProject);
     }
 
 }
