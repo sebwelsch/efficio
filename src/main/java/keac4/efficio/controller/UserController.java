673d7acf5b62ebcc94f9a0932befb01eec8e11c3
sebwelsch <sebjak1@hotmail.com>
2024-12-04 12:43:03 +0100

    css, html and small fixes

--- src/main/java/keac4/efficio/controller/UserController.java
+++ src/main/java/keac4/efficio/controller/UserController.java
@@ -29,20 +29,21 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String saveNewUser(@ModelAttribute User newUser, Model model, RedirectAttributes redirectAttributes) {
+    public String saveNewUser(@ModelAttribute User newUser, RedirectAttributes redirectAttributes) {
         if (userService.findByUsername(newUser.getUsername()) != null) {
-            model.addAttribute("error", "This username is already in use");
+            redirectAttributes.addFlashAttribute("error", "This username is already in use");
             return "redirect:/signup";
         }
 
         // This code compares and confirms the passwords are equal to each other.
         if (!newUser.getPassword().equals(newUser.getConfirmPassword())) {
-            model.addAttribute("error", "Passwords do not match");
-            return "signUp";
+            redirectAttributes.addFlashAttribute("error", "Passwords do not match");
+            return "redirect:/signup";
         }
 
         userService.saveNewUser(newUser);
-        return "redirect:/signup";
+        redirectAttributes.addFlashAttribute("success", "Your account was created! You can now log in");
+        return "redirect:/login";
     }
 
     @GetMapping("/login")
@@ -51,19 +52,19 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String verifyLogin(HttpSession session, @RequestParam String username, @RequestParam String password, Model model, RedirectAttributes redirectAttributes) {
+    public String verifyLogin(HttpSession session, @RequestParam String username, @RequestParam String password, RedirectAttributes redirectAttributes) {
         User user = userService.findByUsername(username);
         if (user != null && userService.authenticate(password, user.getPassword())) {
             session.setAttribute("userSession", user);
             redirectAttributes.addFlashAttribute("success", "You have been logged in");
             return "redirect:/overview";
         }
-        model.addAttribute("error", "Username or password is incorrect, try again.");
-        return "login";
+        redirectAttributes.addFlashAttribute("error", "Username or password is incorrect, try again.");
+        return "redirect:/login";
     }
 
     //Endpoint to invalidate the users sessions
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/login";
